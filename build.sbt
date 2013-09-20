5a55383e556a994144c79e6f9fcb54fc3992b263
Erik Wyatt <wyatte@stolaf.edu>
2013-09-20 14:47:21 -0500

    use scalatest

--- build.sbt
+++ build.sbt
@@ -2,10 +2,10 @@ name := "Akka tutorial"
 
 version := "1.0"
 
-scalaVersion := "2.9.1"
+scalaVersion := "2.10.2"
 
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 
-libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.0"
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.2.1"
 
-
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0.M8"
--- src/main/scala/akka-tutorial/WS.scala
+++ src/main/scala/akka-tutorial/WS.scala
@@ -1,23 +1,24 @@
-//if neighbors maps addresses to actorRefs, then everything else will
-//fall into place
-
 package akkaTutorial
 
 import akka.actor._
 import akka.pattern._
 import akka.util.Timeout
 import scala.collection.mutable
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
-object MyImplicits {
-  implicit val timeout = Timeout(1)
+import org.scalatest.FlatSpec
+
+object Constants {
+  implicit val timeout = Timeout(1 second)
+  val KeyNotFound = new Exception("Key not found")
 }
-import MyImplicits._;
+import Constants._;
  
 
 sealed trait WSMessage
 case class Lookup(address:Int) extends WSMessage
 case class Read(address:Int) extends WSMessage
-case class Found(content:String) extends WSMessage
 case class Redirect(next:ActorRef) extends WSMessage
 case class LoveMe(returnAddress:Int) extends WSMessage
 case class Write(key:Int, value:String) extends WSMessage
@@ -37,54 +38,64 @@ class Node(
       data(key) = value
     case Read(soughtAddress) =>
       if (data contains soughtAddress)
-        sender ! Found(data(soughtAddress))
+        sender ! data(soughtAddress)
       else {
         val distances = (address :: neighbors.keys.toList) map
-          {(a:Int) => math.abs(a - soughtAddress)} zip (self ::
-            (neighbors.keys.toList map neighbors))
+          {(a:Int) => math.abs(a - soughtAddress)} zip (self :: (neighbors.keys.toList map neighbors))
         val nearest = (distances reduce
-          {(e1:Tuple2[Int,ActorRef], e2:Tuple2[Int,ActorRef]) =>
-          if (e1._1 < e2._1) e1 else e2})._2
+          {(e1:Tuple2[Int,ActorRef], e2:Tuple2[Int,ActorRef]) => if (e1._1 < e2._1) e1 else e2}
+          )._2
+        if(nearest == self)
+          sender ! Status.Failure(KeyNotFound)
         sender ! Redirect(nearest)
       }
   }
 }
 
-object Tests {
-  def readSingleNode = {
+object WSSpec extends FlatSpec {
+  def SingleNode = new {
     val system = ActorSystem("WSSystem")
     var data = new collection.mutable.HashMap[Int,String]
-    data(1) = "Hello world"
+    data(1) = "foobar"
     val node = system.actorOf(Props(new Node(1, new mutable.HashMap[Int, ActorRef], data)), name = "node")
-    node ? Read(1) onSuccess {case message =>
-        println(message)
-        system.shutdown()
-      }
   }
 
-  def readMultipleNodes = {
-    val system = ActorSystem("WSSystem")
-    val addresses = 0 until 10
-    val nodes = for(i <- addresses) yield system.actorOf(Props(new
-      Node(i, new mutable.HashMap[Int, ActorRef], new collection.mutable.HashMap[Int,String])), name = "node" + i.toString)
-    val pairs = (nodes.takeRight(1) ++ nodes zip addresses) ++ (nodes zip (addresses.takeRight(1) ++ addresses))
-    pairs foreach {
-      t => t._1 !  LoveMe(t._2)
-    }
-    nodes foreach {
-      node:ActorRef => node ? GetDegree onSuccess {
-        case degree => assert(degree == 2)
-      }
-    }
-    system.shutdown()
+  "A single node" should "read items" in {
+    val sn = SingleNode
+    assert(Await.result(sn.node ? Read(1), 1 second).asInstanceOf[String] == "foobar")
+    sn.system.shutdown()
   }
+
+  it should "throw an exception when asked to read nonexistent items" in {
+    val sn = SingleNode
+    val thrown = intercept[Exception] {
+      sn.node ? Read(2)
+    }
+    assert(thrown === KeyNotFound)
+  }
+
+//  def readMultipleNodes = {
+//    val system = ActorSystem("WSSystem")
+//    val addresses = 0 until 10
+//    val nodes = for(i <- addresses) yield system.actorOf(Props(new
+//      Node(i, new mutable.HashMap[Int, ActorRef], new collection.mutable.HashMap[Int,String])), name = "node" + i.toString)
+//    val pairs = (nodes.takeRight(1) ++ nodes zip addresses) ++ (nodes zip (addresses.takeRight(1) ++ addresses))
+//    pairs foreach {
+//      t => t._1 !  LoveMe(t._2)
+//    }
+//    nodes foreach {
+//      node:ActorRef => node ? GetDegree onSuccess {
+//        case degree => assert(degree == 2)
+//      }
+//    }
+//    //nodes take(5) last ! Write(5, "foobar")
+//    //nodes head ? Read(5) onSuccess
+//    system.shutdown()
+//  }
 }
 
 object WattsStrogatz extends App {
-  println("different text")
-  //assert(0 == 1)
-  Tests.readSingleNode
-  Tests.readMultipleNodes
+  WSSpec.execute()
 }
 
 
