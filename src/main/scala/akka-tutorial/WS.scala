eb8de092a487f36ce1eea3c5578bd15f7444ac34
Erik Wyatt <wyatte@stolaf.edu>
2013-09-20 12:01:42 -0500

    singly-linked ring

--- src/main/scala/akka-tutorial/WS.scala
+++ src/main/scala/akka-tutorial/WS.scala
@@ -1,8 +1,12 @@
+//if neighbors maps addresses to actorRefs, then everything else will
+//fall into place
+
 package akkaTutorial
 
 import akka.actor._
 import akka.pattern._
 import akka.util.Timeout
+import scala.collection.mutable
 
 object MyImplicits {
   implicit val timeout = Timeout(1)
@@ -15,23 +19,29 @@ case class Lookup(address:Int) extends WSMessage
 case class Read(address:Int) extends WSMessage
 case class Found(content:String) extends WSMessage
 case class Redirect(next:ActorRef) extends WSMessage
-case class LoveMe(senderAsNode:Node) extends WSMessage
+case class LoveMe(returnAddress:Int) extends WSMessage
 case class Write(key:Int, value:String) extends WSMessage
+case class GetDegree extends WSMessage
 
-class Node(val address:Int, var neighbors:List[Node], var
-    data:collection.mutable.Map[Int,String]) extends Actor {
+class Node(
+  val address:Int,
+  var neighbors:collection.mutable.Map[Int,ActorRef],
+  var data:collection.mutable.Map[Int,String]
+      ) extends Actor {
   def receive = {
-    case LoveMe(senderAsNode) =>
-      neighbors = senderAsNode :: neighbors
+    case GetDegree =>
+      sender ! neighbors.size
+    case LoveMe(returnAddress) =>
+      neighbors(returnAddress) = sender
     case Write(key, value) =>
       data(key) = value
     case Read(soughtAddress) =>
       if (data contains soughtAddress)
         sender ! Found(data(soughtAddress))
       else {
-        val distances = (address :: (neighbors map {_ address})) map
+        val distances = (address :: neighbors.keys.toList) map
           {(a:Int) => math.abs(a - soughtAddress)} zip (self ::
-            (neighbors map {_ self}))
+            (neighbors.keys.toList map neighbors))
         val nearest = (distances reduce
           {(e1:Tuple2[Int,ActorRef], e2:Tuple2[Int,ActorRef]) =>
           if (e1._1 < e2._1) e1 else e2})._2
@@ -40,13 +50,41 @@ class Node(val address:Int, var neighbors:List[Node], var
   }
 }
 
-object WattsStrogats extends App {
-  val system = ActorSystem("WSSystem")
-  var data = new collection.mutable.HashMap[Int,String]
-  data(1) = "Hello world"
-  val node = system.actorOf(Props(new Node(1, Nil, data)), name = "node")
-  node ? Read(1) onSuccess {case message =>
-      println(message)
-      system.shutdown()
+object Tests {
+  def readSingleNode = {
+    val system = ActorSystem("WSSystem")
+    var data = new collection.mutable.HashMap[Int,String]
+    data(1) = "Hello world"
+    val node = system.actorOf(Props(new Node(1, new mutable.HashMap[Int, ActorRef], data)), name = "node")
+    node ? Read(1) onSuccess {case message =>
+        println(message)
+        system.shutdown()
+      }
+  }
+
+  def readMultipleNodes = {
+    val system = ActorSystem("WSSystem")
+    val addresses = 0 until 10
+    val nodes = for(i <- addresses) yield system.actorOf(Props(new
+      Node(i, new mutable.HashMap[Int, ActorRef], new collection.mutable.HashMap[Int,String])), name = "node" + i.toString)
+    val pairs = (nodes.takeRight(1) ++ nodes zip addresses) ++ (nodes zip (addresses.takeRight(1) ++ addresses))
+    pairs foreach {
+      t => t._1 !  LoveMe(t._2)
     }
+    nodes foreach {
+      node:ActorRef => node ? GetDegree onSuccess {
+        case degree => assert(degree == 2)
+      }
+    }
+    system.shutdown()
+  }
 }
+
+object WattsStrogatz extends App {
+  println("different text")
+  //assert(0 == 1)
+  Tests.readSingleNode
+  Tests.readMultipleNodes
+}
+
+
